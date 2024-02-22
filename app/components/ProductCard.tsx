c3b51f81c07db5fb870a995bcefffb829a43e16d
Gabe Mata <gabemata@gmail.com>
2024-02-22 19:30:09 +0100

    feat: last check in Mallorca

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+// import styles from './ProductCard.module.css';
+
+const ProductCard = () => {
+    return (
+        // <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500' >
+        <div>
+            <button className='btn btn-primary' onClick={() => console.log('Click')}>Add Me</button>
+        </div>
+    )
+}
+
+export default ProductCard
